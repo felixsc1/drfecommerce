7065812a7575415105ef724e741c3dc3bf314548
Felix <felix.schlegel@auticon.ch>
2023-03-29 13:45:15 +0200

    added attribute inline to product admin

--- drfecommerce/product/admin.py
+++ drfecommerce/product/admin.py
@@ -1,68 +1,73 @@
-# from django.contrib import admin
-# from django.urls import reverse
-# from django.utils.safestring import mark_safe
+from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
-# from .models import (
-#     Attribute,
-#     AttributeValue,
-#     Category,
-#     Product,
-#     ProductImage,
-#     ProductLine,
-#     ProductType,
-# )
-
-
-# class EditLinkInline(object):
-#     # see https://docs.djangoproject.com/en/4.1/ref/contrib/admin/#admin-reverse-urls
-#     # for how we got to this url.
-#     # an "instance" is one row in the product_line table.
-#     def edit(self, instance):
-#         url = reverse(
-#             f"admin:{instance._meta.app_label}_{instance._meta.model_name}_change",
-#             args=[instance.pk],
-#         )
-#         if instance.pk:
-#             link = mark_safe(f'<a href="{url}">Edit</a>')
-#             return link
-#         else:
-#             return ""
+from .models import (
+    Attribute,
+    AttributeValue,
+    Category,
+    Product,
+    ProductImage,
+    ProductLine,
+    ProductType,
+)
 
 
-# class ProductLineInline(EditLinkInline, admin.TabularInline):
-#     model = ProductLine
-#     readonly_fields = ("edit",)  # access the edit function within EditLinkInline
+class EditLinkInline(object):
+    # see https://docs.djangoproject.com/en/4.1/ref/contrib/admin/#admin-reverse-urls
+    # for how we got to this url.
+    # an "instance" is one row in the product_line table.
+    def edit(self, instance):
+        url = reverse(
+            f"admin:{instance._meta.app_label}_{instance._meta.model_name}_change",
+            args=[instance.pk],
+        )
+        if instance.pk:
+            link = mark_safe(f'<a href="{url}">Edit</a>')
+            return link
+        else:
+            return ""
 
 
-# # @admin.register(Product)  # alternative way...
-# class ProductAdmin(admin.ModelAdmin):
-#     inlines = [ProductLineInline]
+class ProductLineInline(EditLinkInline, admin.TabularInline):
+    model = ProductLine
+    readonly_fields = ("edit",)  # access the edit function within EditLinkInline
 
 
-# class ProductImageInline(admin.TabularInline):
-#     model = ProductImage
+class AttributeValueInline(admin.TabularInline):
+    # for many-to-many relationship need to specify the related name
+    model = AttributeValue.product_line_attribute_value.through
 
 
-# class AttributeValueInline(admin.TabularInline):
-#     # for many-to-many relationship need to specify the related name
-#     model = AttributeValue.product_line_attribute_value.through
+class AttributeValueProductInline(admin.TabularInline):
+    # for many-to-many relationship need to specify the related name
+    model = AttributeValue.product_attr_value.through
 
 
-# class ProductLineAdmin(admin.ModelAdmin):
-#     inlines = [ProductImageInline, AttributeValueInline]
+# @admin.register(Product)  # alternative way...
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductLineInline, AttributeValueProductInline]
 
 
-# class AttributeInline(admin.TabularInline):
-#     model = Attribute.product_type_attribute.through
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
 
 
-# class ProductTypeAdmin(admin.ModelAdmin):
-#     inlines = [AttributeInline]
+class ProductLineAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline, AttributeValueInline]
 
 
-# admin.site.register(Product, ProductAdmin)
-# admin.site.register(Category)
-# admin.site.register(ProductLine, ProductLineAdmin)
-# admin.site.register(Attribute)
-# admin.site.register(ProductType, ProductTypeAdmin)
-# admin.site.register(AttributeValue)
+class AttributeInline(admin.TabularInline):
+    model = Attribute.product_type_attribute.through
+
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    inlines = [AttributeInline]
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category)
+admin.site.register(ProductLine, ProductLineAdmin)
+admin.site.register(Attribute)
+admin.site.register(ProductType, ProductTypeAdmin)
+admin.site.register(AttributeValue)
